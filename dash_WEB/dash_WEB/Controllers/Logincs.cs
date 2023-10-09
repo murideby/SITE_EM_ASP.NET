b282f083631c24ffc9915892da4ebb89d01ce5ac
Débora Murielle <debora.murielle@gmail.com>
2023-10-08 22:12:43 -0300

    Site - ASP.net

---
+++ dash_WEB/dash_WEB/Controllers/Logincs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dash_WEB.Controllers
+{
+    public class Logincs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ dash_WEB/dash_WEB/Controllers/Suporte_TIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dash_WEB.Controllers
+{
+    public class Suporte_TIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
